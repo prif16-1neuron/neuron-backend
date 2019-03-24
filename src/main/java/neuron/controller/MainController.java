7b50708d12bb817de932ffac83e6309d3eac34c0
alanasjuzokas <alanas@inventi.lt>
2019-03-24 20:31:51 +0200

    chore: change get to post

--- src/main/java/neuron/controller/MainController.java
+++ src/main/java/neuron/controller/MainController.java
@@ -24,7 +24,7 @@ public class MainController {
      * @return calculated result
      */
 
-    @GetMapping(path = "/calculate", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/calculate", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Data> getCalculations(@RequestBody DataRequest body) {
        List<Data> data = body.data.stream()
                .map(d -> new Data(d.getX1(), d.getX2(), d.getW1(), d.getW2()))
