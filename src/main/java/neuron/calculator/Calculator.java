9022bff920e510873a0839d1b8a0d1c3b920e943
alanasjuzokas <alanas@inventi.lt>
2019-04-09 19:58:16 +0300

    feat: auto calc

--- src/main/java/neuron/calculator/Calculator.java
+++ src/main/java/neuron/calculator/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator {
     public List<CalculatedAutoData> autoCalculate(List<AutoData> consumedData, Extra extra){
         List<CalculatedAutoData> calculatedData = new ArrayList<>();
         for(AutoData consumed: consumedData) {
-            CalculatedAutoData data = new CalculatedAutoData(String.valueOf(calculateAutoT(consumed)),"0.31");
+            CalculatedAutoData data = new CalculatedAutoData(String.valueOf(calculateAutoT(consumed,extra)),"0.31");
             calculatedData.add(data);
         }
         return calculatedData;
@@ -57,20 +57,61 @@ public class Calculator {
         return 0;
     }
 
+    private int calculateAutoT(AutoData data){
+        double w0 = data.getW0();
+        double x1 = data.getX1();
+        double x2 = data.getX2();
+        double w1 = data.getW1();
+        double w2 = data.getW2();
+        double x3 = data.getX3();
+        double w3 = data.getW3();
+
+        double sum = x1 * w1 + x2 * w2 + x3 * w3;
+
+        if(sum >= w0) {
+            return 1;
+        }
+        return 0;
+    }
+
     private double convertOfString(String value) {
         return Double.valueOf(value);
     }
 
-    private int calculateAutoT(AutoData data){
-        double w0 = convertOfString(data.getW0());
-        double x1 = convertOfString(data.getX1());
-        double x2 = convertOfString(data.getX2());
-        double w1 = convertOfString(data.getW1());
-        double w2 = convertOfString(data.getW2());
-        double x3 = convertOfString(data.getX3());
-        double w3 = convertOfString(data.getW3());
+    private ArrayList<Double> calculateAutoT(AutoData data, Extra extra){
+        int output;
+        double error = 0;
+        double localError = 0;
+        double rate = extra.getRate();
+        int[] outputs = {1,1,1,1,0,0,0,0};
+        ArrayList<Double> t = new ArrayList<>();
 
-        return 0;
+        double a;
+        int it = 0; //do array of data different and use it for setting w cause it is a string now and cannot be overused
+
+        do {
+            it++;
+            error = 0;
+            for(int i = 0; i < 8; i++){
+                output = calculateAutoT(data);
+                localError = outputs[i] - output;
+                data.setW0(calcW(data.getW0(), rate, localError, data.getX1()));
+                data.setW1(calcW(data.getW0(), rate, localError, data.getX1()));
+                data.setW2(calcW(data.getW0(), rate, localError, data.getX1()));
+                data.setW3(calcW(data.getW0(), rate, localError, data.getX1()));
+
+                error += error * error;
+            }
+            t.add(Math.sqrt(error / 8));
+        } while (error != 0 && it <= extra.getIterations());
+
+
+        return t;
+    }
+
+    private double calcW(double value, double rate, double localError, double x){
+        return value * rate * localError * x;
+
     }
 
 }
--- src/main/java/neuron/entities/AutoData.java
+++ src/main/java/neuron/entities/AutoData.java
@@ -1,15 +1,15 @@
 package neuron.entities;
 
 public class AutoData {
-    private final String w0;
-    private final String x1;
-    private final String x2;
-    private final String w1;
-    private final String w2;
-    private final String w3;
-    private final String x3;
+    private  double w0;
+    private  double x1;
+    private  double x2;
+    private  double w1;
+    private  double w2;
+    private  double w3;
+    private  double x3;
 
-    public AutoData(String w0, String x1, String x2, String w1, String w2, String w3, String x3) {
+    public AutoData(double w0, double x1, double x2, double w1, double w2, double w3, double x3) {
         this.w0 = w0;
         this.x1 = x1;
         this.x2 = x2;
@@ -19,31 +19,60 @@ public class AutoData {
         this.x3 = x3;
     }
 
-    public String getW0() {
+    public double getW0() {
         return w0;
     }
 
-    public String getX1() {
+    public void setW0(double w0) {
+        this.w0 = w0;
+    }
+
+    public double getX1() {
         return x1;
     }
 
-    public String getX2() {
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public double getX2() {
         return x2;
     }
 
-    public String getW1() {
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getW1() {
         return w1;
     }
 
-    public String getW2() {
+    public void setW1(double w1) {
+        this.w1 = w1;
+    }
+
+    public double getW2() {
         return w2;
     }
 
-    public String getW3() {
+    public void setW2(double w2) {
+        this.w2 = w2;
+    }
+
+    public double getW3() {
         return w3;
     }
 
-    public String getX3() {
+    public void setW3(double w3) {
+        this.w3 = w3;
+    }
+
+    public double getX3() {
         return x3;
     }
+
+    public void setX3(double x3) {
+        this.x3 = x3;
+    }
+
 }
